56b3de35c3185fcfbdbce4dc1d7314b55b46a86b
stonex159 <stonex@udel.edu>
2021-09-21 16:05:10 -0400

    imported bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import {member} from './interfaces/member'
+import {Member} from './interfaces/member'
 import './App.css';
 import { hasUncaughtExceptionCaptureCallback } from 'process';
 import { MemberList } from './components/MemberList';
-
+import { ControlPanel } from './components/ControlPanel';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap'
 function App(): JSX.Element {
   return (
-    <div className="App">
+    <Container className="App">
+      <div>
+        <ControlPanel></ControlPanel>
+      </div>
       <div>
         <MemberList></MemberList>
       </div>
-    </div>
+    </Container>
   );
 }
 
