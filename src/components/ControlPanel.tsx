7ea863c2bf3a0878608bb226b2e5726e3d246634
stonex159 <stonex@udel.edu>
2021-09-21 16:03:44 -0400

    Add textboxes, submit-button, and checkbox

---
+++ src/components/ControlPanel.tsx
@@ -0,0 +1,27 @@
+import { Button, Col } from 'react-bootstrap'
+
+export function ControlPanel(): JSX.Element {
+    return <Col>
+    <p>First Name:
+    <input className="textbox" type="text" name="First Name" placeholder="Joe"></input> 
+    </p>
+    <br></br>
+
+    <p>Last Name:
+    <input className="textbox" type="text" name="Last Name" placeholder="Jones"></input>
+    </p>
+    <br></br>
+
+    <p>Email Address:
+    <input className="textbox" type="email" name="Email Address" placeholder="Jjones@udel.edu"></input>
+    </p>
+    <br></br>
+
+    <p>Health Check: 
+    <input className="checkbox" type="checkbox" name="Health Check"></input>
+    </p>
+    <br></br>
+
+    <button id="submit-button"> submit </button>
+    </Col>
+}
