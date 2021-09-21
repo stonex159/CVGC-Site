12920365e8b4a0273cdadb978d9cbfe3915071fe
stonex159 <stonex@udel.edu>
2021-09-21 16:17:00 -0400

    changed divs to bootstrap rows

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,18 @@ import { hasUncaughtExceptionCaptureCallback } from 'process';
 import { MemberList } from './components/MemberList';
 import { ControlPanel } from './components/ControlPanel';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap'
+import { Container, Row } from 'react-bootstrap'
+
+
 function App(): JSX.Element {
   return (
     <Container className="App">
-      <div>
+      <Row>
         <ControlPanel></ControlPanel>
-      </div>
-      <div>
+      </Row>
+      <Row>
         <MemberList></MemberList>
-      </div>
+      </Row>
     </Container>
   );
 }
