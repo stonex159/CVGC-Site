181693089d78fdffbf5a134c815367e590405ed2
stonex159 <stonex@udel.edu>
2021-09-21 20:43:40 -0400

    added a Person class and constructor

--- src/interfaces/member.ts
+++ src/interfaces/member.ts
@@ -1,8 +1,26 @@
 
 export interface Member {
-    ID: number
     first: string
     last: string
     email: string
     status: string
+}
+
+class Person {
+    first: string;
+    last: string;
+    email: string;
+    status: string;
+  
+    constructor(first: string, last: string, email: string, status: string) {
+      this.first = first;
+      this.last = last;
+      this.email = email;
+      this.status = status;
+    }
+  }
+
+export function addMember(member: Member): {first: string; last: string; email: string; status: string} {
+    const person: Member = new Person(member.first, member.last, member.email, member.status);
+    return person;
 }
