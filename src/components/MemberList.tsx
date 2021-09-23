58f3743105385db420487812f7b4022492475f76
stonex159 <stonex@udel.edu>
2021-09-23 16:07:25 -0400

    Shifted Person and addMember to MemberList.tsx

--- src/components/MemberList.tsx
+++ src/components/MemberList.tsx
@@ -1,6 +1,23 @@
 import { Col } from 'react-bootstrap';
 import { Member } from '../interfaces/member';
 
+class Person {
+    name: string;
+    email: string;
+    status: string;
+  
+    constructor(name: string, email: string, status: string) {
+      this.name = name;
+      this.email = email;
+      this.status = status;
+    }
+  }
+
+export function addMember(member: Member): {name: string; email: string; status: string} {
+    const person: Member = new Person(member.name, member.email, member.status);
+    return person;
+}
+
 export function MemberList({member}: {member: Member}): JSX.Element {
     return <Col> 
     <table className="table">
--- src/interfaces/member.ts
+++ src/interfaces/member.ts
@@ -4,20 +4,3 @@ export interface Member {
     email: string
     status: string
 }
-
-class Person {
-    name: string;
-    email: string;
-    status: string;
-  
-    constructor(name: string, email: string, status: string) {
-      this.name = name;
-      this.email = email;
-      this.status = status;
-    }
-  }
-
-export function addMember(member: Member): {name: string; email: string; status: string} {
-    const person: Member = new Person(member.name, member.email, member.status);
-    return person;
-}
